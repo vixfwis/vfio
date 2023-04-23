6f889a154aa56f88c77db14d1be555c6395c3639
vixfwis <vixfwis@gmail.com>
2023-04-23 20:27:12 +0600

    fix wrong cpu pining; add 2 cores

--- pin-cpu.sh
+++ pin-cpu.sh
@@ -2,6 +2,8 @@
 
 set -x
 
-systemctl set-property --runtime -- user.slice AllowedCPUs=0-15
-systemctl set-property --runtime -- system.slice AllowedCPUs=0-15
-systemctl set-property --runtime -- init.scope AllowedCPUs=0-15
+source hooks/qemu.d/win10-vfio/win10-vfio.conf
+
+systemctl set-property --runtime -- user.slice AllowedCPUs=$CORE_RANGE
+systemctl set-property --runtime -- system.slice AllowedCPUs=$CORE_RANGE
+systemctl set-property --runtime -- init.scope AllowedCPUs=$CORE_RANGE
--- unpin-cpu.sh
+++ unpin-cpu.sh
@@ -2,6 +2,8 @@
 
 set -x
 
-systemctl set-property --runtime -- user.slice AllowedCPUs=4-7,12-15
-systemctl set-property --runtime -- system.slice AllowedCPUs=4-7,12-15
-systemctl set-property --runtime -- init.scope AllowedCPUs=4-7,12-15
+source hooks/qemu.d/win10-vfio/win10-vfio.conf
+
+systemctl set-property --runtime -- user.slice AllowedCPUs=$HOST_CORES
+systemctl set-property --runtime -- system.slice AllowedCPUs=$HOST_CORES
+systemctl set-property --runtime -- init.scope AllowedCPUs=$HOST_CORES

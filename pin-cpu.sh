33cbd52deb427a28a1c3b19fcd75e23d326d6381
vixfwis <vixfwis@gmail.com>
2023-04-24 15:17:08 +0600

    pin/unpin scripts were the other way around

--- pin-cpu.sh
+++ pin-cpu.sh
@@ -4,6 +4,6 @@ set -x
 
 source hooks/qemu.d/win10-vfio/win10-vfio.conf
 
-systemctl set-property --runtime -- user.slice AllowedCPUs=$CORE_RANGE
-systemctl set-property --runtime -- system.slice AllowedCPUs=$CORE_RANGE
-systemctl set-property --runtime -- init.scope AllowedCPUs=$CORE_RANGE
+systemctl set-property --runtime -- user.slice AllowedCPUs=$HOST_CORES
+systemctl set-property --runtime -- system.slice AllowedCPUs=$HOST_CORES
+systemctl set-property --runtime -- init.scope AllowedCPUs=$HOST_CORES

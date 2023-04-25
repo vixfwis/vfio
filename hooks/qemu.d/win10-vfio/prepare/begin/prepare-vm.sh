4ef6782e749745aaa879b7ea9299ac0a5a4aece5
vixfwis <vixfwis@gmail.com>
2023-04-25 19:38:38 +0600

    add cpu governor performance

--- hooks/qemu.d/win10-vfio/prepare/begin/prepare-vm.sh
+++ hooks/qemu.d/win10-vfio/prepare/begin/prepare-vm.sh
@@ -6,6 +6,7 @@ source /etc/libvirt/hooks/qemu.d/win10-vfio/win10-vfio.conf
 vfio-isolate \
 	-u $UNDOFILE \
 	drop-caches \
+	cpu-governor performance \
 	cpuset-modify --cpus C$HOST_CORES /system.slice \
 	cpuset-modify --cpus C$HOST_CORES /user.slice \
 	compact-memory \
